3f5e917589fe7466b6b61d6450825c5ff21ac166
Nouran <nourans.96@yahoo.com>
2020-05-05 11:36:59 +0200

    Index page for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-    has_one :category
-    has_one :brand
-    has_one :store
+    belongs_to :category
+    belongs_to :brand
+    belongs_to :store
     has_many :images
     has_and_belongs_to_many :orders
 end
