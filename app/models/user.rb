e4fdbf850189aaf241fd69459c6d20f9c3f11a9c
zeyadsaleh <zeyadmostafasaleh@gmail.com>
2020-05-06 04:32:18 +0200

    full functionality of devise, missing the views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :orders
+
+  validates_presence_of :name # required
+  validates_presence_of :avatar # required
+  has_many :orders
 end
