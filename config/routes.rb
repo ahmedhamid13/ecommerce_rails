9ce1f759174df6356da5d23da09c9c0f6d99dcfd
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-05-07 23:50:38 +0200

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'showcart/', to: 'orders#showCart', as: 'show_cart'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
+  resources :orders
   root 'welcome#index'
 end
