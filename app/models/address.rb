bdae176b0f9d8b3a8adb96a0bced70b4f5623a59
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-05-10 23:39:34 +0200

    generate dummy data using faker

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user
   belongs_to :order
+
+  validates :address, :billing, presence: true,
+              length: { minimum: 5 }
+              
+  validates :address, :billing, presence: true
 end
