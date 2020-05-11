edf32ae27f11ab5d4fe169f4eda9e9845e4970dc
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-05-11 22:54:06 +0200

    show product style

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -23,8 +23,8 @@ class Rate < ApplicationRecord
   end
 
 
-  def check_rate(products, id)
-    (products.rates).where(user_id: id).empty? 
+  def check_rate(product, id)
+    (product.rates).where(user_id: id).empty? 
   end
 
   def get_rate(products, id)
