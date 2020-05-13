59eede25509810c84ca6de7d6aa96ebc675272ff
Nouran <nourans.96@yahoo.com>
2020-05-13 10:18:00 +0200

    Sendgrid Mailer configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Sendgrid mailer
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'app171030440@heroku.com',
+  :password             =>  'msnqkao40395',
+  :domain               =>  'heroku.com',
+  :enable_starttls_auto  =>  true
+}
